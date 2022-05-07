2a744c97ec7aa70137894e016d59e0df96e4e43e
shamil_lisa <97886263+shamil-lol@users.noreply.github.com>
2022-05-07 22:37:19 +0530

    Update _plugin.js

--- plugins/_plugin.js
+++ plugins/_plugin.js
@@ -14,7 +14,7 @@ const got = require('got');
 const fs = require('fs');
 const Db = require('./sql/plugin');
 
-const Language = require('../language');
+const Language = require('language');
 const Lang = Language.getString('_plugin');
 const NLang = Language.getString('updater');
 
